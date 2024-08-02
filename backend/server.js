592a62420d92175a66c57294c80815a32c593ba8
Akshat Pandey <akshatp1903@gmail.com>
2024-08-02 21:13:29 +0530

    Added chat API

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,25 @@
-const express = require("express")
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
-const app = express()
+const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("API is Running")
-})
+    res.send("API is Running Successfully");
+});
 
-app.listen(5000, console.log("Server started on port 5000"));
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+app.get('/api/chat/:id', (req, res) => {
+    //console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000, console.log('Server started on 5000'));
 
